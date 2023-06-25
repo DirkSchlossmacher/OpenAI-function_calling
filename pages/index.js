77bdb84973412b2f7783657bb543a62084ca1090
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-06-25 08:54:52 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,50 @@ import { useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { FunctionForm } from '../components/FunctionForm';
 
+// New Hook for managing localStorage
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
 const Home = () => {
-  const methods = useForm({ defaultValues: { functions: [{ name: '', description: '', parameters: [] }] } });
+  const initialFormState = { functions: [{ name: '', description: '', parameters: [] }] };
+  const methods = useForm({ defaultValues: initialFormState });
   const { handleSubmit, control, register } = methods;
 
-  const [apiResponse, setApiResponse] = useState(null);
-  const [userPrompt, setUserPrompt] = useState("");
-  const [apiKey, setApiKey] = useState(localStorage.getItem("OPENAI_KEY") || "");
+  const [apiResponse, setApiResponse] = useLocalStorage('apiResponse', null);
+  const [userPrompt, setUserPrompt] = useLocalStorage('userPrompt', "");
+  const [apiKey, setApiKey] = useLocalStorage('OPENAI_KEY', "");
 
   const onSubmit = (data) => {
     // Submit the data to your /api/completions.ts function.
@@ -18,10 +55,12 @@ const Home = () => {
 
   const handleApiKeyChange = (event) => {
     setApiKey(event.target.value);
-    localStorage.setItem("OPENAI_KEY", event.target.value);
   }
 
-  
+  const handleUserPromptChange = (event) => {
+    setUserPrompt(event.target.value);
+  }
+
   return (
     <div>
       <FormProvider {...methods}>
@@ -29,7 +68,7 @@ const Home = () => {
           <input 
             type="text"
             value={userPrompt}
-            onChange={(event) => setUserPrompt(event.target.value)}
+            onChange={handleUserPromptChange}
             placeholder="Enter your user message"
           />
           <input 
