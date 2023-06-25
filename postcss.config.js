5c9c51dac75a198ba3eedc4785ad8f74438a68d4
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-06-25 08:31:49 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+  ],
+  sourceMap: true,
+  importLoaders: 1,
+  importFrom: ['./styles/globals.css']
 }
